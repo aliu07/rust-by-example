25e2298b07a2293ba13ac812b59a1a40ec3bc579
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-09 21:03:38 -0400

    Add dummy function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5);
+
+    another_different_function();
 }
 
 fn another_function(x: i32) {
     println!("Another function. The value of x is: {x}");
+}
+
+fn another_different_function() {
+    another_function(5)
 }
