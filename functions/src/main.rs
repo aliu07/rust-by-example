84c32e0f33195137386dc86e63edf60486480be7
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-10 22:53:42 -0400

    Add dummy comments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,10 +6,12 @@ fn main() {
     another_different_function();
 }
 
+// This is another function
 fn another_function(x: i32) {
     println!("Another function. The value of x is: {x}");
 }
 
+// This is another different function
 fn another_different_function() {
     another_function(5)
 }
