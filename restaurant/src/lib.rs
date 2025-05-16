cf1706905ee0cd340b707c7cc7dda2daf5f2b53f
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-16 12:52:51 -0400

    Add extra modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,7 +1,13 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    // Need to add pub to module too since private by default
+    // Module is only container. We also need to make its contents
+    // i.e. specific functions public if we want to access them
+    // from other modules
+    pub mod hosting {
+        // This function is accessible
+        pub fn add_to_waitlist() {}
 
+        // This function is private by default
         fn seat_at_table() {}
     }
 
@@ -14,17 +20,54 @@ mod front_of_house {
     }
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        // Super = .. in file system
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn eat_at_restaurant() {
+    // Abs path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let _ = back_of_house::Appetizer::Soup;
+    let _ = back_of_house::Appetizer::Salad;
 }
