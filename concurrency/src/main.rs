63ce991b7c62bc3cb8f4ae1779a4a955c17e4944
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-21 23:58:23 -0400

    Add channel and message passing examples

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,12 +1,17 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     interleaved_example();
-
     println!("=================================");
-
     transferring_ownership_example();
+    println!("=================================");
+    channel_example();
+    println!("=================================");
+    sending_multiple_messages_example();
+    println!("=================================");
+    multiple_producers_example();
 }
 
 fn interleaved_example() {
@@ -36,4 +41,91 @@ fn transferring_ownership_example() {
     });
 
     handle.join().unwrap()
+}
+
+fn channel_example() {
+    // mpsc = multiple producer, single consumer
+    // tx = transmitter
+    // rx = receiver
+    let (tx, rx) = mpsc::channel();
+
+    // Spawned thread needs to own 'tx', hence the move keyword
+    thread::spawn(move || {
+        let val = String::from("Hi!");
+        println!("Sending '{val}' from spawned thread");
+        // Ownership of 'val' is transferred when calling send().
+        tx.send(val).unwrap();
+        // Uncomment this line and you will realize it won't compile.
+        // println!("val is {val}");
+    });
+
+    // recv() blocks the main thread's execution and waits until a value
+    // is sent down the channel. Best if we need to wait to continue.
+    //
+    // try_recv() is another method that returns immediately a Result<T, E>.
+    // Better suited if we can do other things while waiting for a value
+    // to be received through the channel.
+    let received = rx.recv().unwrap();
+    println!("Got '{received}' in main thread");
+}
+
+fn sending_multiple_messages_example() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
+
+fn multiple_producers_example() {
+    let (tx, rx) = mpsc::channel();
+
+    // Clone the transmitter for first spawned thread
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    // Transfer ownership of original transmitter into 2nd spawned thread
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
